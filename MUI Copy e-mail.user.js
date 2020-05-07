806ee47c6a6565868a0dd610f81a7f6f5c4f6a5f
Bocon Gabriel <gabriel.bocon@volvo.com>
2020-05-07 15:07:54 -0300

    Initial commit

---
+++ MUI Copy e-mail.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         MUI Copy e-mail
+// @namespace    mui_copy_email
+// @version      0.2
+// @description  Grabs the email when you click the object.
+// @author       Gabriel Boçon
+// @match        https://secure.volvo.com/mgmui/User/*
+// @grant        GM_notification
+// @run-at       document-idle
+// ==/UserScript==
+
+//var MutationObserver = window.MutationObserver;
+var myObserver = new MutationObserver (function(){
+    console.log("modificou");
+    addListener();
+});
+var elementToObserve = document.querySelector("#ContentPlaceHolder1_UpdPanelSearch");
+
+myObserver.observe (elementToObserve, {subtree: true, childList: true});
+
+addListener();
+
+function addListener() {
+    var emailObj = document.getElementById("ContentPlaceHolder1_GeneralData1_TxtPrimaryMailAddress");
+
+    if (emailObj){
+        console.log("adicionando listener");
+        emailObj.addEventListener('click', function(event) {
+            if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+                var textarea = document.createElement("textarea");
+                textarea.textContent = emailObj.value;
+                textarea.style.position = "fixed";
+                document.body.appendChild(textarea);
+                textarea.select();
+                try {
+                    return document.execCommand("copy");
+                }
+                catch (ex) {
+                    console.warn("Copy to clipboard failed.", ex);
+                    return false;
+                }
+                finally {
+                    document.body.removeChild(textarea);
+                    GM_notification ( {title: 'E-mail copied!', text: 'The e-mail ' + emailObj.value + ' has been copied!'} );
+                }
+            }
+        });
+    }
+}
